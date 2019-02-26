6c26d07ebbfc85f9f543ba958627c1bcbe533ea0
mollinaca <github@mail.watarinohibi.tokyo>
2019-02-26 22:34:52 +0900

    add euclidean.bash ユークリッドの互除法をbashで

---
+++ euclidean.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+a="${1}"
+b="${2}"
+
+echo "A=${a}"
+echo "B=${b}"
+
+if [ "${b}" -gt "${a}" ]; then
+    echo "input A(arg1) bigger than B(arg2)" 1>&2
+    exit 1
+fi    
+
+r=1
+while true;
+do
+    r=$((a%b))
+    echo "A mod B = ${r}"
+    if [ "${r}" -eq 0 ]; then
+        break
+    fi
+    b="${r}"
+done
+
+echo "${a}の最大公約数は${b}"
