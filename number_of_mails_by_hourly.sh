ab49f653f2711f6ad5c79d8e153fd85a25b5792e
mollinaca <github@mail.watarinohibi.tokyo>
2019-08-25 14:04:32 +0900

    add script

---
+++ number_of_mails_by_hourly.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TARGET_TIME="$(date '+%d %H' -d '1 hour ago'):"
+
+number_of_sent=$(grep "${TARGET_TIME}" /var/log/maillog | grep "status=sent" | wc -l)
+number_of_reject=$(grep "${TARGET_TIME}" /var/log/maillog | grep "NOQUEUE: reject: RCPT" | wc -l)
+
+## notification
+function slack {
+    local webhook_url="https://hooks.slack.com/services/xxxxxxxxx/yyyyyyyyy/zzzzzzzzzzzzzzzzzzzzzzzz"
+
+    TIME=$(date '+%Y/%m/%d %H' -d '1 hour ago')
+    text="number of mails around ${TIME}:xx
+    sent    : ${number_of_sent}
+    rejected: ${number_of_reject}"
+
+    curl -sS -X POST --data-urlencode \
+      "payload={\"text\": \"\`\`\`${text}\`\`\`\", \
+                \"username\": \"mailServer_checker\", \
+                \"icon_emoji\": \":mail:\"}" \
+                "${webhook_url}" >/dev/null 2>&1
+}
+
+slack
